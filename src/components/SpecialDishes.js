a5932f40a2bd0415db4a8c3372560371ca26bc79
dileepk706 <dlpkmr706@gmail.com>
2023-05-30 19:44:31 +0530

    active class added

--- src/components/SpecialDishes.js
+++ src/components/SpecialDishes.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import Items from "./items"
+import Categories from "./category";
+import Pagination from "./pagination";
 
 const SpecialDishes=(props)=>
 {
     let [searchItems,searchItemsState]=useState([])
     let [loading,loadingState]=useState(true)
-    console.log('props');
+    let[crrPage,crrPageState]=useState(1)
+    let[pagePerWidow,pagePerWidowStae]=useState(6)
    
-    let flag=0
     const search=async(api)=>{
         loadingState(true)
         let r= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${api}`)
@@ -27,14 +29,23 @@ const SpecialDishes=(props)=>
             loadingState(false)
         }
    
+
+      
+
+        let firsIndx=crrPage*pagePerWidow-pagePerWidow
+        let lastIndx=crrPage*pagePerWidow
+   
+        let newItems=props.dishes.slice(firsIndx,lastIndx)
+
     
-    let items= props.dishes.map(e => {
+    let items=newItems.map(e => {
         return (
             <Items
                 items={e}
             />
         )
     })
+  
     let sResult=searchItems.map(e=>{
         return (
             <div className="searchItem">
@@ -64,46 +75,58 @@ const SpecialDishes=(props)=>
         )
     })
     return(
-        <div className="special">
+        <div>
+            <div className="special">
 
-            <div className="special-dishes-content">
-                <h1>our specials</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ea quibusdam veritatis numquam nostrum facere nisi rem aperiam, deserunt est. Neque dicta similique iste necessitatibus omnis facere ex eius laboriosam!</p>
-            </div>
-            <div className="items">
-                {items}
-            </div>
-
-            <br />
-            <div className="">
-                <div className="search">
-                    <input type="text" name="" id="g" placeholder="search here" />
-                    <button onClick={async () => {
-                        let d = document.getElementById('g').value
-                        search(d)
-                    }}>searhc by name</button>
-
+                <div className="special-dishes-content">
+                    <h1>our specials</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ea quibusdam veritatis numquam nostrum facere nisi rem aperiam, deserunt est. Neque dicta similique iste necessitatibus omnis facere ex eius laboriosam!</p>
                 </div>
+                <div className="items">
+                    {/* display top item */}
+                    {items}
+                </div>
+                {/* pagination */}
+                <Pagination items={props.dishes} currentpage={crrPageState} pages={pagePerWidow} />
 
-                <div className="searchItems">
-                    <div className="filter-section">
-                        <h2>filter by category</h2>
 
-                        <div className="category-buttons">
-                            {buttons}
+                <div className="">
+                    <div className="search">
+                        <input type="text" name="" id="g" placeholder="search here" />
+                        <button onClick={async () => {
+                            let d = document.getElementById('g').value
+                            search(d)
+                        }}>searhc by name</button>
+
+                    </div>
+
+
+
+                    <div className="searchItems">
+                        <div className="filter-section">
+                            <h2>filter by category</h2>
+
+                            <div className="category-buttons">
+                                {buttons}
+                            </div>
                         </div>
-                    </div>
-                    <div className="items-section">
-                    {!loading?sResult: <h3>loading...</h3>} 
-                    </div>
-                    
+                        <div className="items-section">
+                            {!loading ? sResult : <h3>loading...</h3>}
+                        </div>
 
+
+                    </div>
                 </div>
+
+
+
             </div>
 
-
+            <Categories items={items} />
 
         </div>
+
+        
     )
 }
 
